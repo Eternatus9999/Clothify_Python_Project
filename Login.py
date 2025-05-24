60f583d4d5c31c473ba12237e2e708abcbfd8f6a
Eternatus9999 <eternatus9999@gmail.com>
2025-05-24 09:57:40 +0530

    create classes and add login button a function to navigate to the 2nd window

--- Login.py
+++ Login.py
@@ -1,27 +1,32 @@
 from customtkinter import *
 
-root = CTk()
+class Login:
+    def __init__(self, master):
+        self.master = master
+        root = CTk()
 
-root.title("Clothify")
+        root.title("Clothify")
 
-root.geometry("600x400")
+        root.geometry("600x400")
 
-root.config(bg="#000000")
+        root.resizable(False, False)
 
-login_button = CTkButton(master=root,width=200, height=50, text="Login", font=("Arial", 20), fg_color="#00FFFF", text_color="#000000", corner_radius=40, bg_color="#000000")
+        root.config(bg="#000000")
 
-back_button = CTkButton(master=root,width=20, height=30, text="←", font=("Arial", 20), fg_color="#000000", text_color="#00FFFF", corner_radius=40, bg_color="#000000",  hover_color="gray")
+        login_button = CTkButton(master=root,width=200, height=50, text="Login", font=("Arial", 20), fg_color="#00FFFF", text_color="#000000", corner_radius=40, bg_color="#000000")
 
-title = CTkLabel(master=root, text="CLOTHIFY", font=("Luckiest Guy", 80, "bold"), fg_color="#000000", text_color="#00FFFF", bg_color="#000000")
+        back_button = CTkButton(master=root,width=20, height=30, text="←", font=("Arial", 20), fg_color="#000000", text_color="#00FFFF", corner_radius=40, bg_color="#000000",  hover_color="gray")
 
-email = CTkEntry(master=root, placeholder_text="Email", width=300, height=50, fg_color="#FFFFFF", text_color="#000000", font=("Arial", 20), corner_radius=40, bg_color="#000000")
+        title = CTkLabel(master=root, text="CLOTHIFY", font=("Luckiest Guy", 80, "bold"), fg_color="#000000", text_color="#00FFFF", bg_color="#000000")
 
-password = CTkEntry(master=root, placeholder_text="Password", show="*", width=300, height=50, fg_color="#FFFFFF", text_color="#000000", font=("Arial", 20), corner_radius=40, bg_color="#000000")
+        email = CTkEntry(master=root, placeholder_text="Email", width=300, height=50, fg_color="#FFFFFF", text_color="#000000", font=("Arial", 20), corner_radius=40, bg_color="#000000")
 
-title.place(relx=0.5, rely=0.1, anchor=CENTER)
-back_button.place(relx=0.1, rely=0.3, anchor=CENTER)
-email.place(relx=0.5, rely=0.4, anchor=CENTER)
-password.place(relx=0.5, rely=0.6, anchor=CENTER)
-login_button.place(relx=0.5, rely=0.8, anchor=CENTER)
+        password = CTkEntry(master=root, placeholder_text="Password", show="*", width=300, height=50, fg_color="#FFFFFF", text_color="#000000", font=("Arial", 20), corner_radius=40, bg_color="#000000")
 
-root.mainloop()
+        title.place(relx=0.5, rely=0.1, anchor=CENTER)
+        back_button.place(relx=0.1, rely=0.3, anchor=CENTER)
+        email.place(relx=0.5, rely=0.4, anchor=CENTER)
+        password.place(relx=0.5, rely=0.6, anchor=CENTER)
+        login_button.place(relx=0.5, rely=0.8, anchor=CENTER)
+
+        root.mainloop()
--- Main.py
+++ Main.py
@@ -1,19 +1,35 @@
 from customtkinter import *
 
-root = CTk()
+class Main:
+    def __init__(self):
+        root = CTk()
 
-root.title("Clothify")
+        root.title("Clothify")
 
-root.geometry("600x400")	
+        root.geometry("600x400")	
 
-root.config(bg="#000000")
+        root.eval('tk::PlaceWindow . center')
 
-register_button = CTkButton(master=root,width=200, height=50, text="Register", font=("Arial", 20), fg_color="#00FFFF", text_color="#000000", corner_radius=40, bg_color="#000000")
-login_button = CTkButton(master=root, width=200, height=50, text="Login", font=("Arial", 20), fg_color="#FFFFFF", text_color="#000000", corner_radius=40, bg_color="#000000")
+        root.resizable(False, False)
 
-title = CTkLabel(master=root, text="CLOTHIFY", font=("Luckiest Guy", 80, "bold"), fg_color="#000000", text_color="#00FFFF", bg_color="#000000")
+        root.config(bg="#000000")
 
-register_button.place(relx=0.5, rely=0.5, anchor=CENTER)
-title.place(relx=0.5, rely=0.3, anchor=CENTER)
-login_button.place(relx=0.5, rely=0.7, anchor=CENTER)
-root.mainloop()
+
+        def login():
+            root.destroy()
+            import Login
+            Login.Login(root)
+
+
+        register_button = CTkButton(master=root,width=200, height=50, text="Register", font=("Arial", 20), fg_color="#00FFFF", text_color="#000000", corner_radius=40, bg_color="#000000")
+        login_button = CTkButton(master=root, width=200, height=50, text="Login", font=("Arial", 20), fg_color="#FFFFFF", text_color="#000000", corner_radius=40, bg_color="#000000", command=login)   
+
+        title = CTkLabel(master=root, text="CLOTHIFY", font=("Luckiest Guy", 80, "bold"), fg_color="#000000", text_color="#00FFFF", bg_color="#000000")
+
+        register_button.place(relx=0.5, rely=0.5, anchor=CENTER)
+        title.place(relx=0.5, rely=0.3, anchor=CENTER)
+        login_button.place(relx=0.5, rely=0.7, anchor=CENTER)
+
+        root.mainloop()
+
+Main()
