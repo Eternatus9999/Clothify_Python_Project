0f1e59cb31973491a012a42d3c9bca4d3cda8f58
Eternatus9999 <eternatus9999@gmail.com>
2025-06-01 10:08:03 +0530

    connect database to the main page interface

--- Main_Page.py
+++ Main_Page.py
@@ -1,6 +1,8 @@
 from customtkinter import *
 from tkinter import messagebox
 
+import DatabaseConnector
+
 class MainPage:
     def __init__(self):
         
@@ -16,7 +18,7 @@ class MainPage:
 
         title = CTkLabel(master=self.root, text="CLOTHIFY", font=("Luckiest Guy", 80, "bold"), fg_color="#000000", text_color="#00FFFF", bg_color="#000000")
         
-        self.txtUser = CTkEntry(master=self.root, placeholder_text="Username", width=300, height=50, fg_color="#FFFFFF", text_color="#000000", font=("Arial", 20), corner_radius=40, bg_color="#000000")
+        self.txtUser = CTkEntry(master=self.root, placeholder_text="UserId", width=300, height=50, fg_color="#FFFFFF", text_color="#000000", font=("Arial", 20), corner_radius=40, bg_color="#000000")
         self.txtPassword = CTkEntry(master=self.root, placeholder_text="Password", show="*", width=300, height=50, fg_color="#FFFFFF", text_color="#000000", font=("Arial", 20), corner_radius=40, bg_color="#000000")
 
         login_button = CTkButton(master=self.root,width=200, height=50, text="Login", font=("Arial", 20), fg_color="#00FFFF", text_color="#000000", corner_radius=40, bg_color="#000000", command=self.login)
@@ -29,18 +31,24 @@ class MainPage:
         self.root.mainloop()
 
     def login(self):
-            email = self.txtUser.get()
+            userid = int(self.txtUser.get())
             password = self.txtPassword.get()
-            if(email == "admin" and password == "admin"):
+            if(userid == "admin" and password == "admin"):
                 self.root.destroy()
                 import Admin
                 Admin.Admin()
-            elif(email == "cashier" and password == "cashier"):
+            elif(self.isEmployee(userid, password)):
                 self.root.destroy()
                 import Chashier
                 Chashier.Cashier()
             else:
-                print("Invalid Email or Password")
                 self.txtUser.delete(0, END)
                 self.txtPassword.delete(0, END)
                 messagebox.showerror("Error", "Invalid Email or Password")
+
+    def isEmployee(self, userid, password):
+        employees = DatabaseConnector.viewEmployee()
+        for employee in employees:
+            if(employee[0] == userid and employee[5] == password):
+                return True
+        return False
