3abecc444cc5923a5fcfe2cba5fb40c644eb8639
Eternatus9999 <eternatus9999@gmail.com>
2025-06-02 19:34:08 +0530

    connect the database to the view order interface

--- View_Order.py
+++ View_Order.py
@@ -1,10 +1,12 @@
 from customtkinter import *
 from tkinter import ttk
 
+import DatabaseConnector
+
 class ViewOrder:
     def __init__(self,root):
 
-        self.orders = []
+        self.orders = DatabaseConnector.viewOrder()
 
         frame = CTkFrame(master= root, width=1000, height=680, fg_color="#A09E9E")
 
@@ -12,11 +14,19 @@ class ViewOrder:
 
         delete_button = CTkButton(master=frame, width=20, height=30, text="DELETE", font=("Arial", 30, 'bold'), fg_color="#FF0000", text_color="#000000", corner_radius=40, command=self.delete)
 
-        self.table = ttk.Treeview(frame, height=20, columns=("ID", "Customer Name", "Total Price"), show="headings")
+        self.table = ttk.Treeview(frame, height=20, columns=("ID", "Customer Name", "Payment Method", "Price", "Date"), show="headings")
 
         self.table.heading("ID", text="ID")
         self.table.heading("Customer Name", text="Customer Name")
-        self.table.heading("Total Price", text="Total Price")
+        self.table.heading("Payment Method", text="Payment Method")
+        self.table.heading("Price", text="Price")
+        self.table.heading("Date", text="Date")
+
+        self.table.column("ID", anchor=CENTER)
+        self.table.column("Customer Name", anchor=CENTER)
+        self.table.column("Payment Method", anchor=CENTER)
+        self.table.column("Price", anchor=CENTER)
+        self.table.column("Date", anchor=CENTER)
 
         for order in self.orders:
             self.table.insert("", "end", values=order)
@@ -32,4 +42,5 @@ class ViewOrder:
     def delete(self):
             selected_item = self.table.selection()
             for item in selected_item:
+                DatabaseConnector.deleteOrder(self.table.item(item)['values'][0])
                 self.table.delete(item)
