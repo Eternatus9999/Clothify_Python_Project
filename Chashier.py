9abe23946cb186bdd1315dfd6f2b4c29a473911e
Eternatus9999 <eternatus9999@gmail.com>
2025-06-09 13:21:49 +0530

    remove add and update products and suppliers from cashier interface

--- Chashier.py
+++ Chashier.py
@@ -22,12 +22,8 @@ class Cashier:
         add_order_button = CTkButton(master=self.root, width=225, height=30, text="Add Order", font=("Arial", 26, 'bold'), fg_color="#00FFFF", text_color="#000000", corner_radius=40, bg_color="#000000", command = self.addOrder)
         view_order_button = CTkButton(master=self.root, width=225, height=30, text="View Order", font=("Arial", 26, 'bold'), fg_color="#00FFFF", text_color="#000000", corner_radius=40, bg_color="#000000", command= self.viewOrder)
         
-        add_product_button = CTkButton(master=self.root, width=225, height=30, text="Add Product", font=("Arial", 26, 'bold'), fg_color="#00FFFF", text_color="#000000", corner_radius=40, bg_color="#000000", command = self.addProduct)
-        update_product_button = CTkButton(master=self.root, width=20, height=30, text="Update Product", font=("Arial", 26, 'bold'), fg_color="#00FFFF", text_color="#000000", corner_radius=40, bg_color="#000000", command= self.updateProduct)
         view_product_button = CTkButton(master=self.root, width=225, height=30, text="View Product", font=("Arial", 26, 'bold'), fg_color="#00FFFF", text_color="#000000", corner_radius=40, bg_color="#000000", command= self.viewProduct)
         
-        add_supplier_button = CTkButton(master=self.root, width=225, height=30, text="Add Supplier", font=("Arial", 26, 'bold'), fg_color="#00FFFF", text_color="#000000", corner_radius=40, bg_color="#000000", command= self.addSupplier)
-        update_supplier_button = CTkButton(master=self.root, width=225, height=30, text="Update Supplier", font=("Arial", 26, 'bold'), fg_color="#00FFFF", text_color="#000000", corner_radius=40, bg_color="#000000", command= self.updateSupplier)
         view_supplier_button = CTkButton(master=self.root, width=225, height=30, text="View Supplier", font=("Arial", 26, 'bold'), fg_color="#00FFFF", text_color="#000000", corner_radius=40, bg_color="#000000", command= self.viewSupplier)
 
         title.place(relx=0.5, rely=0.06, anchor=CENTER)
@@ -37,13 +33,9 @@ class Cashier:
         add_order_button.place(relx=0.1, rely=0.2, anchor=CENTER)
         view_order_button.place(relx=0.1, rely=0.26, anchor=CENTER)
 
-        add_product_button.place(relx=0.1, rely=0.34, anchor=CENTER)
-        update_product_button.place(relx=0.1, rely=0.4, anchor=CENTER)
-        view_product_button.place(relx=0.1, rely=0.46, anchor=CENTER)
+        view_product_button.place(relx=0.1, rely=0.34, anchor=CENTER)
 
-        add_supplier_button.place(relx=0.1, rely=0.54, anchor=CENTER)
-        update_supplier_button.place(relx=0.1, rely=0.6, anchor=CENTER)
-        view_supplier_button.place(relx=0.1, rely=0.66, anchor=CENTER)
+        view_supplier_button.place(relx=0.1, rely=0.4, anchor=CENTER)
 
         self.root.mainloop()
 
@@ -55,23 +47,7 @@ class Cashier:
     def addOrder(self):
         import Add_Order
         Add_Order.AddOrder(self.frame)
-
-    def addProduct(self):
-        import Add_Product
-        Add_Product.AddProduct(self.frame)
-
-    def addSupplier(self):
-        import Add_Supplier
-        Add_Supplier.AddSupplier(self.frame)
-
-    def updateProduct(self):
-        import Update_Product
-        Update_Product.UpdateProduct(self.frame)
-
-    def updateSupplier(self):
-        import Update_Supplier
-        Update_Supplier.UpdateSupplier(self.frame)
-        
+    
     def viewProduct(self):
         import View_Product
         View_Product.ViewProduct(self.frame)
