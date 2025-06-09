d5aaab2d2ada0bc1c040efcd8e245771973b25d9
Eternatus9999 <eternatus9999@gmail.com>
2025-06-09 13:23:22 +0530

    justify the content of the table

--- View_Supplier.py
+++ View_Supplier.py
@@ -21,6 +21,11 @@ class ViewSupplier:
         self.table.heading("Company", text="Email")
         self.table.heading("Contact", text="Contact")
 
+        self.table.columns("ID", anchor=CENTER)
+        self.table.columns("Name", anchor=CENTER)
+        self.table.columns("Company", anchor=CENTER)
+        self.table.columns("Contact", anchor=CENTER)
+
         for supplier in self.suppliers:
             self.table.insert("", "end", values=supplier)
         
